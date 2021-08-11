ec43225561e7d7781f18a3ee342a72def6911752
directoryx <anggawijaya4567@gmail.com>
2021-08-11 17:25:10 +0700

    Create Blog Server

--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -1,20 +1,78 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net"
 	"os"
 	"os/signal"
+	"time"
 
 	"go-grpc-mongo/blog/blogproto"
 
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/status"
 )
+
+var collection *mongo.Collection
 
 type server struct{}
 
+func (s *server) CreateBlog(ctx context.Context, req *blogproto.CreateBlogRequest) (*blogproto.CreateBlogResponse, error) {
+	blog := req.GetBlog()
+
+	data := &blogItem{
+		AuthorID: blog.AuthorId,
+		Title:    blog.Title,
+		Content:  blog.Content,
+	}
+
+	res, err := collection.InsertOne(ctx, data)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, fmt.Sprint("Error : %v", err))
+	}
+
+	objId, errMongo := res.InsertedID.(primitive.ObjectID)
+
+	if !errMongo {
+		return nil, status.Errorf(codes.Internal, fmt.Sprint("Error : %v", errMongo))
+	}
+
+	return &blogproto.CreateBlogResponse{
+		Blog: &blogproto.Blog{
+			Id:       objId.Hex(),
+			AuthorId: blog.AuthorId,
+			Title:    blog.Title,
+			Content:  blog.Content,
+		},
+	}, nil
+}
+
+type blogItem struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	AuthorID string             `bson:"author_id"`
+	Title    string             `bson:"title"`
+	Content  string             `bson:"content"`
+}
+
 func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://mongo:27017"))
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+		fmt.Println("Disconnecting MongoDB")
+	}()
+
+	collection = client.Database("mydb").Collection("blog")
+
 	lis, err := net.Listen("tcp", "0.0.0.0:8010")
 	if err != nil {
 		panic(err)
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module go-grpc-mongo
 go 1.16
 
 require (
+	go.mongodb.org/mongo-driver v1.7.1 // indirect
 	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
 	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e // indirect
 	golang.org/x/text v0.3.7 // indirect
